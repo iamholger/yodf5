53e2f6b9e83e17fd0c816b670bdd114b7f5df110
Holger Schulz <hschulz@fnal.gov>
2020-05-02 20:02:14 -0500

    Index function that only reads the AOs and their number bins from file, also Eigen version of get_field

--- readAndWrite.cc
+++ readAndWrite.cc
@@ -5,9 +5,11 @@
 #include <unordered_map>
 #include <functional>
 
+#include "Eigen/Dense"
 
 #include "boost/multi_array.hpp"
 #define H5_USE_BOOST
+#define H5_USE_EIGEN
 #include <highfive/H5Easy.hpp>
 #include <highfive/H5DataSet.hpp>
 #include <highfive/H5DataSpace.hpp>
@@ -15,7 +17,143 @@
 
 using namespace YODA;
 using namespace std;
+using namespace Eigen;
 using namespace HighFive;
+
+
+unordered_map< string, unordered_map< string, int> > myyodaindex(istream& stream_) {
+
+    unordered_map<string, unordered_map< string, int>> hmap;
+    hmap.insert({"Histo1D",   unordered_map< string, int>()});
+    hmap.insert({"Histo2D",   unordered_map< string, int>()});
+    hmap.insert({"Profile1D", unordered_map< string, int>()});
+    hmap.insert({"Profile2D", unordered_map< string, int>()});
+    hmap.insert({"Scatter1D", unordered_map< string, int>()});
+    hmap.insert({"Scatter2D", unordered_map< string, int>()});
+    hmap.insert({"Scatter3D", unordered_map< string, int>()});
+    hmap.insert({"Counter",   unordered_map< string, int>()});
+
+    //#ifdef HAVE_LIBZ
+    //// NB. zstr auto-detects if file is deflated or plain-text
+    //zstr::istream stream(stream_);
+    //#else
+    istream& stream = stream_;
+    //#endif
+    enum Context
+    {
+        NONE, SCATTER1D, SCATTER2D, SCATTER3D, COUNTER, HISTO1D, HISTO2D,
+        PROFILE1D, PROFILE2D
+    };
+
+
+    /// State of the parser: line number, line, parser context, and pointer(s) to the object currently being assembled
+    unsigned int nline = 0;
+    string s;
+    Context context = NONE;
+    std::string curpath="";
+    int nbins=0;
+
+    // Loop over all lines of the input file
+    bool in_anns = false;
+    string fmt = "1";
+    while (std::getline(stream, s))
+    {
+        nline += 1;
+        if (!in_anns)
+        {
+            Utils::itrim(s);
+            if (s.empty()) continue;
+            if (s.find("#") == 0 && s.find("BEGIN") == string::npos && s.find("END") == string::npos) continue;
+        }
+        if (context == NONE)
+        {
+            if (s.find("BEGIN ") == string::npos)
+            {
+                stringstream ss;
+                ss << "Unexpected line in YODA format parsing when BEGIN expected: '" << s << "' on line " << nline;
+                throw ReadError(ss.str());
+            }
+            while (s.find("#") == 0) s = Utils::trim(s.substr(1));
+            vector<string> parts;
+            istringstream iss(s); string tmp;
+            while (iss >> tmp) parts.push_back(tmp);
+
+            if (parts.size() < 2 || parts[0] != "BEGIN")
+            {
+                stringstream ss;
+                ss << "Unexpected BEGIN line structure when BEGIN expected: '" << s << "' on line " << nline;
+                throw ReadError(ss.str());
+            }
+            const string ctxstr = parts[1];
+            curpath = (parts.size() >= 3) ? parts[2] : "";
+            nbins=0;
+            if      (Utils::startswith(ctxstr, "YODA_COUNTER"))   {context = COUNTER;  }
+            else if (Utils::startswith(ctxstr, "YODA_SCATTER1D")) {context = SCATTER1D;}
+            else if (Utils::startswith(ctxstr, "YODA_SCATTER2D")) {context = SCATTER2D;}
+            else if (Utils::startswith(ctxstr, "YODA_SCATTER3D")) {context = SCATTER3D;}
+            else if (Utils::startswith(ctxstr, "YODA_HISTO1D"))   {context = HISTO1D;  }
+            else if (Utils::startswith(ctxstr, "YODA_HISTO2D"))   {context = HISTO2D;  }
+            else if (Utils::startswith(ctxstr, "YODA_PROFILE1D")) {context = PROFILE1D;}
+            else if (Utils::startswith(ctxstr, "YODA_PROFILE2D")) {context = PROFILE2D;}
+            const size_t vpos = ctxstr.find_last_of("V");
+            fmt = vpos != string::npos ? ctxstr.substr(vpos+1) : "1";
+            if (fmt != "1") in_anns = true;
+        }
+        else
+        { //< not a BEGIN line
+            if (s.find("BEGIN ") != string::npos) throw ReadError("Unexpected BEGIN line in YODA format parsing before ending current BEGIN..END block");
+            // FINISHING THE CURRENT CONTEXT
+            if (s.find("END ") != string::npos)
+            {
+                if      (context == HISTO1D)   {hmap["Histo1D"]  .insert({curpath, nbins});}
+                else if (context == HISTO2D)   {hmap["Histo2D"]  .insert({curpath, nbins});}
+                else if (context == PROFILE1D) {hmap["Profile1D"].insert({curpath, nbins});}
+                else if (context == PROFILE2D) {hmap["Profile2D"].insert({curpath, nbins});}
+                else if (context == SCATTER1D) {hmap["Scatter1D"].insert({curpath, nbins});}
+                else if (context == SCATTER2D) {hmap["Scatter2D"].insert({curpath, nbins});}
+                else if (context == SCATTER3D) {hmap["Scatter3D"].insert({curpath, nbins});}
+                else if (context == COUNTER)   {hmap["Counter"]  .insert({curpath, nbins});}
+                in_anns = false;
+                context = NONE;
+                continue;
+            }
+            // ANNOTATIONS PARSING
+            if (fmt == "1")
+            {
+                // First convert to one-key-per-line YAML syntax
+                const size_t ieq = s.find("=");
+                if (ieq != string::npos) s.replace(ieq, 1, ": ");
+                // Special-case treatment for syntax clashes
+                const size_t icost = s.find(": *");
+                if (icost != string::npos)
+                {
+                    s.replace(icost, 1, ": '*");
+                    s += "'";
+                }
+                // Store reformatted annotation
+                const size_t ico = s.find(":");
+                if (ico != string::npos) continue;
+            }
+            else if (in_anns)
+            {
+                if (s == "---") in_anns = false;
+                continue;
+            }
+
+            if ( (context == HISTO1D) || (context==HISTO2D) || (context==PROFILE1D) || (context==PROFILE2D))
+            {
+                if (s.find("Total") != string::npos || s.find("Underflow") != string::npos || s.find("Overflow") != string::npos) continue;
+                nbins++;
+            }
+            else if ( (context==SCATTER1D)  || (context==SCATTER2D) || (context==SCATTER3D))
+            {
+                nbins++;
+            }
+        }
+    }
+    return hmap;
+}
+
 
 
 // pre C++20
@@ -25,7 +163,8 @@ inline bool ends_with(string const & value, string const & ending)
     return equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
-inline vector<string> get_ao_names(const vector<AnalysisObject*>& aos, string const & aotype) {
+inline vector<string> get_ao_names(const vector<AnalysisObject*>& aos, string const & aotype)
+{
     vector<string> aonames;
     for (auto ao : aos) 
     {
@@ -36,7 +175,8 @@ inline vector<string> get_ao_names(const vector<AnalysisObject*>& aos, string co
 }
 
 
-inline vector<string> get_variations(const vector<AnalysisObject*>& aos, const string & hname) {
+inline vector<string> get_variations(const vector<AnalysisObject*>& aos, const string & hname)
+{
     vector<string> vars= {""};
     for (auto ao : aos) 
     {
@@ -52,7 +192,8 @@ inline vector<string> get_variations(const vector<AnalysisObject*>& aos, const s
     return vars;
 }
 
-unordered_map<string, int> mk_nbinmap(unordered_map<string, AnalysisObject* > aomap, vector<string> const & hnames) {
+unordered_map<string, int> mk_nbinmap(unordered_map<string, AnalysisObject* > aomap, vector<string> const & hnames)
+{
     unordered_map<string, int> nbin_map;
     for (auto hn : hnames) {
         nbin_map.insert({hn, dynamic_cast<Histo1D*>(aomap[hn])->numBins()});
@@ -106,7 +247,6 @@ inline vector<double> get_edge(unordered_map<string, AnalysisObject* > aomap, ve
     return edges;
 }
 
-// TODO add variant that uses Eigen::ArrayXXd
 boost::multi_array<double,2>  get_h1d_field(
         unordered_map<string, AnalysisObject* > aomap, 
         vector<string> const & hnames, vector<string> const & vars, size_t nbins,
@@ -144,12 +284,52 @@ boost::multi_array<double,2>  get_h1d_field(
     return field;
 }
 
+
+MatrixXd get_h1d_field_eig(
+        unordered_map<string, AnalysisObject* > aomap,
+        vector<string> const & hnames, vector<string> const & vars, size_t nbins,
+        unordered_map<string, int>  nbinsMap,
+        double (Dbn1D::*function)() const,
+        double (HistoBin1D::*function2)() const
+        )
+{
+    MatrixXd field(vars.size(), nbins);
+    size_t offset(0);
+    for (auto  hn : hnames)
+    {
+        for (size_t j=0; j<vars.size();++j)
+        {
+            std::string varhname = (j>0) ? hn+"["+vars[j]+"]" : hn;
+            if (aomap.count(varhname)>0)
+            {
+                auto h = dynamic_cast<Histo1D*>(aomap[varhname]);
+                field(j, offset  ) = (h->totalDbn().*function)();
+                field(j, offset+1) = (h->overflow().*function)();
+                field(j, offset+2) = (h->underflow().*function)();
+                for (int nb=0;nb< h->numBins();++nb) field(j, offset + 3 + nb) = (h->bin(nb).*function2)();
+                if (j==vars.size()-1) offset += h->numBins() + 3;
+            }
+            else
+            {
+                field(j, offset  ) = 0.0;
+                field(j, offset+1) = 0.0;
+                field(j, offset+2) = 0.0;
+                for (int nb=0;nb< nbinsMap[hn];++nb) field(j, offset + 3 + nb) = 0.0;
+                if (j==vars.size()-1) offset += nbinsMap[hn] + 3;
+            }
+        }
+    }
+    return field;
+}
+ 
+
 int main(int argc, const char** argv)
 {
     int compression=atoi(argv[1]);
     std::cerr << argv[1] << "\n";
     size_t nFiles=argc-2;
     std::cerr << argv[2] << " "<< nFiles << "\n";
+
     auto aos = ReaderYODA::create().read(argv[2]);
     unordered_map<string, AnalysisObject*> aomap;
     for (auto ao : aos) aomap.insert({ao->path(), ao});
@@ -158,7 +338,16 @@ int main(int argc, const char** argv)
     auto const h1d_names = get_ao_names(aos, "Histo1D");
     auto const binids    = mk_binids(aomap, h1d_names);
     auto const vars      = get_variations(aos, h1d_names[0]);
-    auto nbinmap = mk_nbinmap(aomap, h1d_names);
+
+
+    std::ifstream instream;
+    instream.open(argv[2]);
+    // This is a map of AOtype : {hname: nbins} --- runs 10x faster than read -- good for initial survey when
+    // running over many files
+    auto hmap = myyodaindex(instream);
+    instream.close();
+    auto nbinmap = hmap["Histo1D"];
+
 
     H5Easy::File file("example.h5", H5Easy::File::ReadWrite | H5Easy::File::Create| H5Easy::File::Truncate);
     
@@ -192,7 +381,7 @@ int main(int argc, const char** argv)
     // Dummy here, we write the data of the first input file as many times as there are command line arguments
     for (size_t fidx=0; fidx<NF; ++fidx)
     {
-        file.getDataSet("/Histo1D/sumW").select(       {0, 0, fidx}, {NB, NV, 1}).write(get_h1d_field(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumW,       &HistoBin1D::sumW));
+        file.getDataSet("/Histo1D/sumW").select(       {0, 0, fidx}, {NB, NV, 1}).write(get_h1d_field_eig(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumW,       &HistoBin1D::sumW));
         file.getDataSet("/Histo1D/sumW2").select(      {0, 0, fidx}, {NB, NV, 1}).write(get_h1d_field(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumW2,      &HistoBin1D::sumW2));
         file.getDataSet("/Histo1D/sumWX").select(      {0, 0, fidx}, {NB, NV, 1}).write(get_h1d_field(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumWX,      &HistoBin1D::sumWX));
         file.getDataSet("/Histo1D/sumWX2").select(     {0, 0, fidx}, {NB, NV, 1}).write(get_h1d_field(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumWX2,     &HistoBin1D::sumWX2));
