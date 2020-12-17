6c0febae47f962cd33e3991107e40b7d317f0ed7
Holger Schulz <holger.schulz@durham.ac.uk>
2020-12-17 13:49:01 +0000

    Cleanup and fully working iplementation of dumping Histo1D to h5 via xtensor

--- testxtensor.cxx
+++ testxtensor.cxx
@@ -4,7 +4,13 @@
 #include <functional>
 
 #include <xtensor/xtensor.hpp>
+#include <xtensor/xarray.hpp>
 #include <xtensor/xrandom.hpp>
+#include <xtensor/xio.hpp>
+
+#include "Eigen/Dense"
+
+#define H5_USE_EIGEN
 
 #define H5_USE_XTENSOR
 #include <highfive/H5Easy.hpp>
@@ -12,9 +18,110 @@
 #include <highfive/H5DataSpace.hpp>
 #include <highfive/H5File.hpp>
 
+#include "YODA/Histo1D.h"
+#include "YODA/ReaderYODA.h"
+
 using namespace std;
+using namespace YODA;
+using namespace Eigen;
 using namespace HighFive;
 
+// Pre C++20 this is needed
+inline bool ends_with(string const & value, string const & ending)
+{
+    if (ending.size() > value.size()) return false;
+    return equal(ending.rbegin(), ending.rend(), value.rbegin());
+}
+
+inline bool starts_with(string const & value, string const & pattern)
+{
+  if (value.rfind(pattern, 0) == 0)
+  {
+    return true;
+  }
+  return false;
+}
+
+
+
+inline vector<string> get_ao_names(const vector<AnalysisObject*>& aos, string const & aotype)
+{
+    vector<string> aonames;
+    for (auto ao : aos) 
+    {
+        if (ao->type()==aotype &! ::ends_with(ao->path(), string(1, ']'))) aonames.push_back(ao->path());
+    }
+    sort(aonames.begin(), aonames.end());
+    return aonames;
+}
+
+inline vector<string> get_variations(const vector<AnalysisObject*>& aos, const string & hname)
+{
+    vector<string> vars= {""};
+    for (auto ao : aos) 
+    {
+        if ( ::starts_with(ao->path(), hname+"[") && ::ends_with(ao->path(), string(1, ']')))
+        {
+            string s(ao->path());
+            s.erase(s.begin(), s.begin()+hname.length()+1);
+            s.erase(s.end()-1);
+            vars.push_back(s);
+        }
+    }
+    sort(vars.begin(), vars.end());
+    return vars;
+}
+
+inline vector<string> mk_binids(unordered_map<string, AnalysisObject* > aomap, vector<string> const & hnames) 
+{
+    vector<string> binids;
+    for (auto hn : hnames) 
+    {
+        binids.push_back(hn+"#T");
+        binids.push_back(hn+"#O");
+        binids.push_back(hn+"#U");
+        for (int nb=0;nb<dynamic_cast<Histo1D*>(aomap[hn])->numBins();++nb) binids.push_back(hn+"#" + to_string(nb));
+    }
+    return binids;
+}
+
+
+// We need similar functions for the other AO types
+// TODO can this be simplified?
+void fill_h1d_field_xt(
+        unordered_map<string, AnalysisObject* > aomap,
+        vector<string> const & hnames, vector<string> const & vars, size_t nbins,
+        unordered_map<string, int>  nbinsMap,
+        double (Dbn1D::*function)() const,
+        double (HistoBin1D::*function2)() const, xt::xtensor<double, 3> & target, size_t zindex
+        )
+{
+    size_t offset(0);
+    for (auto  hn : hnames)
+    {
+        for (size_t j=0; j<vars.size();++j)
+        {
+            std::string varhname = (j>0) ? hn+"["+vars[j]+"]" : hn;
+            if (aomap.count(varhname)>0)
+            {
+                auto h = dynamic_cast<Histo1D*>(aomap[varhname]);
+                target(j, offset  , zindex) = (h->totalDbn().*function)();
+                target(j, offset+1, zindex) = (h->overflow().*function)();
+                target(j, offset+2, zindex) = (h->underflow().*function)();
+                for (int nb=0;nb< h->numBins();++nb) target(j, offset + 3 + nb, zindex) = (h->bin(nb).*function2)();
+                if (j==vars.size()-1) offset += h->numBins() + 3;
+            }
+            else
+            {
+                target(j, offset  , zindex) = 0.0;
+                target(j, offset+1, zindex) = 0.0;
+                target(j, offset+2, zindex) = 0.0;
+                for (int nb=0;nb< nbinsMap[hn];++nb) target(j, offset + 3 + nb, zindex) = 0.0;
+                if (j==vars.size()-1) offset += nbinsMap[hn] + 3;
+            }
+        }
+    }
+}
 
 int main(int argc, const char** argv)
 {
@@ -22,23 +129,55 @@ int main(int argc, const char** argv)
 
     H5Easy::File file("example.h5", H5Easy::File::ReadWrite | H5Easy::File::Create| H5Easy::File::Truncate);
     
-
-    const size_t nbins = 10000;
-    const size_t nvars = 250;
-    const size_t nobjs = 10;
   
-    xt::xtensor<double,3> E = xt::zeros<double>({nbins,nvars,nobjs});
-    //xt::xtensor<double,3> E = xt::random::rand<double>({nbins,nvars,nobjs});
+    // Read all objects into a map
+    auto aos = ReaderYODA::create().read(argv[2]);
+    unordered_map<string, AnalysisObject*> aomap;
+    for (auto ao : aos) aomap.insert({ao->path(), ao});
+
+    // yuck
+    std::ifstream instream;
+    instream.open(argv[2]);
+    auto amap = ReaderYODA::create().mkIndex(instream);
+
+    // TODO this should be a loop over all AnalysisObject types in the current yoda file --- or later what
+    // is know to the rivet analysis handler in terms of AOs
+    // We will have one big h5 dataset for each AO type
+
+      // Determines sorting order --- in a robust application all hnames must be known
+      auto const h1d_names = get_ao_names(aos, "Histo1D");
+      auto const binids    = ::mk_binids(aomap, h1d_names);
+      auto const vars      = get_variations(aos, h1d_names[0]);
+    
+
+      // TODO check what this does
+      auto nbinmap = amap.getAOIndex()["Histo1D"];
+
+      // Dataset extents
+      size_t NB = binids.size();
+      size_t NV = vars.size();
 
 
-    H5Easy::dump(file, "E", E, H5Easy::DumpOptions(H5Easy::Compression(compression)));
+      xt::xtensor<double, 3>::shape_type sh1 = {NB, NV, 5};
+      auto a1 = xt::empty<double>(sh1);
+      fill_h1d_field_xt(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumW      ,    &HistoBin1D::sumW,        a1, 0);
+      fill_h1d_field_xt(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumW2     ,    &HistoBin1D::sumW2,       a1, 1);
+      fill_h1d_field_xt(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumWX     ,    &HistoBin1D::sumWX,       a1, 2);
+      fill_h1d_field_xt(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::sumWX2    ,    &HistoBin1D::sumWX2,      a1, 3);
+      fill_h1d_field_xt(aomap, h1d_names, vars, NB, nbinmap, &Dbn1D::numEntries,    &HistoBin1D::numEntries,  a1, 4);
+      H5Easy::dump(file, "Histo1D", a1, H5Easy::DumpOptions(H5Easy::Compression(compression)));
 
-    HighFive::DataSet ds = file.getDataSet("E");
-
-    std::string string_list("these are some attributes");
-
-    Attribute a = ds.createAttribute<std::string>("attributename", DataSpace::From(string_list));
-    a.write(string_list);
-
+      // TODO make the attribute creation a function    
+      HighFive::DataSet ds = file.getDataSet("Histo1D");
+      Attribute a = ds.createAttribute<std::string>("binids", DataSpace::From(binids));
+      a.write(binids);
+      Attribute b = ds.createAttribute<std::string>("histonames", DataSpace::From(h1d_names));
+      b.write(h1d_names);
+      Attribute c = ds.createAttribute<std::string>("weightnames", DataSpace::From(vars));
+      c.write(vars);
+      vector<string> zinfo = {"sumW", "sumW2", "sumWX", "sumWX2", "numEntries"};
+      Attribute d = ds.createAttribute<std::string>("zinfo", DataSpace::From(zinfo));
+      d.write(zinfo);
+      
     return 0;
 }
